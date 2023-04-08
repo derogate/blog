f17c545f7330ba35c9501a1bf454c2ace8049475
derogate <afiqf.as@gmail.com>
2023-04-08 18:16:18 +0800

    chore: socials & prose AppConfig

---
+++ blog/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  socials: {
+    github: 'derogate'
+  },
+  prose: {
+    h1: {
+      icon: 'line-md:beer-alt-twotone-loop'
+    },
+    h2: { icon: 'material-symbols:link-rounded' },
+    h3: { icon: 'material-symbols:link-rounded' },
+    h4: { icon: 'material-symbols:link-rounded' },
+  }
+})
