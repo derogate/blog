d4d4a3a70d3c7f79b04a3be3d4c7be37580c9914
derogate <afiqf.as@gmail.com>
2023-04-08 20:18:22 +0800

    chore: add Tailwind darkMode & colors extend config

---
+++ blog/tailwind.config.ts
@@ -0,0 +1,13 @@
+import colors from 'tailwindcss/colors'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.gray
+      }
+    }
+  }
+}
