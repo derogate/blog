1c76c042d829ac5c7dcaf2eca77a2a398f54ea8e
derogate <afiqf.as@gmail.com>
2023-04-09 16:10:46 +0800

    style: JIT height include layout padding

--- blog/components/content/DocumentDrivenEmpty.vue
+++ blog/components/content/DocumentDrivenEmpty.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-[calc(100vh-(var(--navbar-height,60px)))] text-center">
+  <div class="flex flex-col items-center justify-center h-[calc(100vh-(2.5rem*2)-(var(--navbar-height,60px)))] text-center">
     <Icon name="line-md:alert-twotone" class="text-yellow-500 text-7xl" />
     <p class="text-4xl">Page is empty</p>
     <NuxtLink to="/" class="border-b border-b-teal-500 border-dashed hover:border-solid hover:border-b-teal-500 hover:text-teal-500 mt-2">Back to Homepage</NuxtLink>
