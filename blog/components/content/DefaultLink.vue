9b764fdac53b5afe01c7a0e1e5aeb23461ed4f40
derogate <afiqf.as@gmail.com>
2023-04-09 18:25:06 +0800

    feat: DefaultLink for NuxtLink wrapper

---
+++ blog/components/content/DefaultLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="border-b border-b-teal-500 border-dashed hover:border-solid hover:border-b-teal-500 hover:text-teal-500"
+    v-bind="$attrs">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+import { NuxtLinkProps } from '#app';
+
+interface Props {
+  to: NuxtLinkProps['to'];
+}
+defineProps<Props>();
+</script>
