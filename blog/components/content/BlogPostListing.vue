d433b439171325121ef4c22dc627351273361d42
derogate <afiqf.as@gmail.com>
2023-04-16 12:01:26 +0800

    refactor: static style, post load state

--- blog/components/content/BlogPostListing.vue
+++ blog/components/content/BlogPostListing.vue
@@ -1,25 +1,41 @@
 <template>
   <ClientOnly>
-    <DefaultLink
-      v-for="post in posts" 
-      :key="post._id"
-      :to="post._path"
-      class="w-fit group">
-      <span :title="`Published on: ${post.publishedOn ?? '-'}. Last updated on: ${post.lastUpdated ?? '-'}`" class="text-xs text-gray-600 dark:text-gray-400 group-hover:text-inherit leading-[1] mr-2">
-        {{ post.lastUpdated ?? post.publishedOn }}
-      </span>
-      <span>
-        {{ post.title }}
-      </span>
-    </DefaultLink>
+    <Icon v-if="isLoading" name="svg-spinners:3-dots-move" />
+    <p v-else-if="!isLoading && !posts" class="text-red-500">
+      <Icon name="line-md:alert-twotone" class="text-orange-500 w-6 h-6" />
+      Unable to get posts
+    </p>
+    <div v-else-if="!isLoading && posts && posts?.length > 0">
+      <DefaultLink
+        v-for="post in posts" 
+        :key="post._id"
+        :to="post._path"
+        class="w-fit group">
+        <span :title="`Published on: ${post.publishedOn ?? '-'}. Last updated on: ${post.lastUpdated ?? '-'}`" class="text-xs text-gray-600 dark:text-gray-400 group-hover:text-inherit leading-[1] mr-2">
+          {{ post.lastUpdated ?? post.publishedOn }}
+        </span>
+        <span>
+          {{ post.title }}
+        </span>
+      </DefaultLink>
+    </div>
   </ClientOnly>
 </template>
 
 <script lang="ts" setup>
-const posts = await queryContent("posts")
-  .where({
-    _empty: false,
-    _draft: false,
-  })
-  .find();
+import { ParsedContent } from '@nuxt/content/dist/runtime/types';
+
+const isLoading = ref(true);
+const posts = ref<ParsedContent[]>();
+
+onBeforeMount(async() => {
+  posts.value = await queryContent("posts")
+    .where({
+      _empty: false,
+      _draft: false,
+    })
+    .find();
+
+  isLoading.value = false;
+})
 </script>
--- blog/components/content/ColorModeSwitch.vue
+++ blog/components/content/ColorModeSwitch.vue
@@ -2,11 +2,11 @@
   <button aria-label="Color Mode" class="flex transition dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300" @click="onClick">
     <ColorScheme placeholder="..." tag="div" class="w-6 h-6">
       <template v-if="colorMode.value === 'dark'">
-        <Icon name="line-md:sunny-filled-loop-to-moon-filled-transition" class="w-5 h-5" />
+        <Icon name="ph:moon" class="w-5 h-5" />
         <span class="sr-only">Dark Mode</span>
       </template>
       <template v-else>
-        <Icon name="line-md:moon-to-sunny-outline-transition" class="w-6 h-6" />
+        <Icon name="ph:sun" class="w-6 h-6" />
         <span class="sr-only">Light Mode</span>
       </template>
     </ColorScheme>
