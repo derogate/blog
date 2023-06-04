d41dc14819628183e8d925dc9b8f72a717fe9793
derogate <afiqf.as@gmail.com>
2023-06-04 11:35:07 +0800

    refactor: sort latest first, check for date

--- blog/components/content/BlogPostListing.vue
+++ blog/components/content/BlogPostListing.vue
@@ -5,14 +5,15 @@
       <Icon name="line-md:alert-twotone" class="text-orange-500 w-6 h-6" />
       Unable to get posts
     </p>
-    <div v-else-if="!isLoading && posts && posts?.length > 0">
+    <div v-else-if="!isLoading && posts && posts?.length > 0" class="flex flex-col">
       <DefaultLink
         v-for="post in posts" 
         :key="post._id"
         :to="post._path"
         class="w-fit group">
-        <span :title="`Published on: ${post.publishedOn ?? '-'}. Last updated on: ${post.lastUpdated ?? '-'}`" class="text-xs text-gray-600 dark:text-gray-400 group-hover:text-inherit leading-[1] mr-2">
-          {{ post.lastUpdated ?? post.publishedOn }}
+        <span v-if="post.publishedOn || post.lastUpdated" :title="`Published on: ${post.publishedOn ?? '-'}. Last updated on: ${post.lastUpdated ?? '-'}`" 
+        class="relative text-xs text-gray-600 dark:text-gray-400 group-hover:text-inherit leading-[1] mr-2">
+          <span class="absolute top-[-2px] left-[-3px] group-hover:text-white transition">{{ post.lastUpdated ? '*' : '' }}</span>{{ post.lastUpdated ?? post.publishedOn }}
         </span>
         <span>
           {{ post.title }}
@@ -34,6 +35,8 @@ onBeforeMount(async() => {
       _empty: false,
       _draft: false,
     })
+    .sort({ publishedOn: -1, $numeric: true })
+    .sort({ lastUpdated: -1, $numeric: true })
     .find();
 
   isLoading.value = false;
